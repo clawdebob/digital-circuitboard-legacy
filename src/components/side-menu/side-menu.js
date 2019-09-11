49a9779a0cc6a0ce947bac540df1e2f71da52453
clawdebob <mok938@gmail.com>
2019-09-11 23:20:49 +0300

    slitly working slide menu

--- src/components/side-menu/side-menu.js
+++ src/components/side-menu/side-menu.js
@@ -1,28 +1,56 @@
 import React from 'react';
 
-var min = 300;
-var max = 3600;
-
 class sideMenu extends React.Component {
-    handleDrag(e) {
-        e.preventDefault();
-        console.log(12);
-        document.getElementById('side-menu').style.width = 1200;
+    constructor(props) {
+        super(props);
+        this.slide = {
+            column: null,
+            curColumnWidth: null,
+            pageX: null,
+            width: null,
+        };
+        this.handleMouseDown = this.handleMouseDown.bind(this);
 
-        // document.addEventListener('mousemove', (e) => {
-        //     e.preventDefault();
-        //     var x = e.pageX - document.getElementById('side-menu').offsetLeft;
-        //     document.getElementById('side-menu').style.width = 1200;
-        //     // if (x > min && x < max && e.pageX < window.innerWidth) {
-        //     //      = x;
-        //     // }
-        // });
+        const slide = this.slide;
+
+        document.addEventListener('mousemove', (e) => {
+            console.log(slide.width);
+            if (slide.column) {
+                let diffX = e.pageX - slide.pageX;
+                if (slide.column.getBoundingClientRect().width > slide.width
+                || (slide.column.getBoundingClientRect().width === slide.width && diffX > 0)) {
+                    slide.column.style.width = `${diffX + slide.curColumnWidth}px`;
+                } else {
+                    slide.column.style.width = `${slide.width}px`;
+                }
+            }
+        });
+
+        document.addEventListener('mouseup', (e) => {
+            slide.column = undefined;
+            slide.pageX = undefined;
+            slide.curColumnWidth = undefined;
+            slide.width = undefined;
+        });
+    }
+
+
+    handleMouseDown(e) {
+        const slide = this.slide;
+
+        slide.column = e.target.parentElement;
+        slide.pageX = e.pageX;
+        slide.curColumnWidth = slide.column.offsetWidth;
+        slide.width = e.target.offsetWidth + 2;
     }
 
     render() {
         return (
             <div className="side-menu" id="side-menu">
-                <div id="split-bar" onMouseDown={this.handleDrag}/>
+                <div
+                    id="split-bar"
+                    onMouseDown={this.handleMouseDown}
+                />
             </div>
         );
     }
