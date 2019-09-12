7dfd547d9730367a9419b4b2563a29c3a1378fc2
clawdebob <mok938@gmail.com>
2019-09-13 00:37:10 +0300

    optimized handlers & styles

--- src/components/side-menu/side-menu.js
+++ src/components/side-menu/side-menu.js
@@ -3,35 +3,38 @@ import React from 'react';
 class sideMenu extends React.Component {
     constructor(props) {
         super(props);
+
         this.slide = {
             column: null,
             curColumnWidth: null,
             pageX: null,
             width: null,
-        };
-        this.handleMouseDown = this.handleMouseDown.bind(this);
-
-        const slide = this.slide;
-
-        document.addEventListener('mousemove', (e) => {
-            console.log(slide.width);
-            if (slide.column) {
-                let diffX = e.pageX - slide.pageX;
-                if (slide.column.getBoundingClientRect().width > slide.width
-                || (slide.column.getBoundingClientRect().width === slide.width && diffX > 0)) {
-                    slide.column.style.width = `${diffX + slide.curColumnWidth}px`;
-                } else {
-                    slide.column.style.width = `${slide.width}px`;
+            mouseMove(e) {
+                if (this.column) {
+                    let diffX = e.pageX - this.pageX;
+                    if (this.column.getBoundingClientRect().width > this.width
+                        || (this.column.getBoundingClientRect().width === this.width && diffX > 0)) {
+                        this.column.style.width = `${diffX + this.curColumnWidth}px`;
+                    } else {
+                        this.column.style.width = `${this.width}px`;
+                    }
                 }
-            }
-        });
+            },
+            mouseUp() {
+                this.column = undefined;
+                this.pageX = undefined;
+                this.curColumnWidth = undefined;
+                this.width = undefined;
+                document.removeEventListener('mouseup', this.mouseUp);
+                document.removeEventListener('mousemove', this.mouseMove);
+            },
+        };
 
-        document.addEventListener('mouseup', (e) => {
-            slide.column = undefined;
-            slide.pageX = undefined;
-            slide.curColumnWidth = undefined;
-            slide.width = undefined;
-        });
+        let slide = this.slide;
+
+        slide.mouseMove = slide.mouseMove.bind(slide);
+        slide.mouseUp = slide.mouseUp.bind(slide);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
     }
 
 
@@ -42,15 +45,19 @@ class sideMenu extends React.Component {
         slide.pageX = e.pageX;
         slide.curColumnWidth = slide.column.offsetWidth;
         slide.width = e.target.offsetWidth + 2;
+
+        document.addEventListener('mousemove', slide.mouseMove);
+        document.addEventListener('mouseup', slide.mouseUp);
     }
 
     render() {
         return (
-            <div className="side-menu" id="side-menu">
+            <div className="side-menu">
                 <div
-                    id="split-bar"
+                    className="side-menu__split-bar"
                     onMouseDown={this.handleMouseDown}
                 />
+                <div className="side-menu__list"></div>
             </div>
         );
     }
